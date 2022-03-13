70441ecf7da13d66f81c4f9ee3640ac5105c3099
lokomotivbj <lokomotiv-bj@yandex.ru>
2022-03-13 11:34:43 +0300

    added fullscreen mode

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 
 
+
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
 
@@ -12,14 +13,18 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_heigth)
-        )
+        if self.settings.fullscreen_mode:
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            self.settings.screen_width = self.screen.get_rect().width
+            self.settings.screen_height = self.screen.get_rect().height
+        else:
+            self.screen = pygame.display.set_mode(
+                (self.settings.screen_width, self.settings.screen_heigth)
+            )
 
         pygame.display.set_caption('Alien Invasion')
 
         self.ship = Ship(self)
-
 
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -27,25 +32,33 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_screen()
-            
 
     def _check_events(self):
         """Обрабатывает нажатия клавиш и события мыши"""
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
-                
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    if event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
 
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Реагирует на нажатия клавиш"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Реагирует на отпускание клавиш"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
--- settings.py
+++ settings.py
@@ -8,3 +8,4 @@ class Settings:
         self.screen_heigth = 600
         self.bg_color = (56, 27, 91)
         self.ship_speed = 1.5
+        self.fullscreen_mode = True
