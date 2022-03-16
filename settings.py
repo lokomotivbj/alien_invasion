e8df49ce54f6b37927d9de0629292a095becaf57
lokomotivbj <lokomotiv-bj@yandex.ru>
2022-03-16 10:47:47 +0300

    added base shooting feauture

--- settings.py
+++ settings.py
@@ -9,3 +9,7 @@ class Settings:
         self.bg_color = (56, 27, 91)
         self.ship_speed = 1.5
         self.fullscreen_mode = True
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
